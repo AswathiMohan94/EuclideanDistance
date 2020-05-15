84e89c0e9083313681b369e978ee2209886c08a2
AswathiMohan94 <aswathimohantly@gmail.com>
2020-05-15 17:09:12 +0530

    [Aswathi]REFACTORED : refactored the class name as per the naming convension

--- src/main/java/com/bridgelabz/distanceCalculator/Distance.java
+++ src/main/java/com/bridgelabz/distanceCalculator/Distance.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.distanceCalculator;
 
-public class DistanceCalculator {
+public class Distance {
 
     public double findEuclideanDistance(double firstXCoordinate1,double firstYCoordinate1, double secondXCoordinate2,double secondYCoordinate2) {
         double euclideanDistance = Math.sqrt((secondYCoordinate2 - firstYCoordinate1) * (secondYCoordinate2 - firstYCoordinate1) + (secondXCoordinate2 - firstXCoordinate1) * (secondXCoordinate2 - firstXCoordinate1));
         return euclideanDistance;
     }
     public static void main(String[] args){
-        DistanceCalculator euclideanDistance= new DistanceCalculator();
+        Distance euclideanDistance= new Distance();
         System.out.println(euclideanDistance.findEuclideanDistance(2,4,3,8)) ;
     }
 }
